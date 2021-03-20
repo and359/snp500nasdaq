1ffa435180e39b99ac17d41d34b4e4446680c195
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 00:15:47 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,8 +4,8 @@ if(isset($_POST["tweet"]))
 {
  $connect = mysqli_connect("us-cdbr-east-03.cleardb.com", "b11d6e54534643", "318fd8ce", "heroku_a7bcbc3dd84756e");
  $tweet = mysqli_real_escape_string($connect, $_POST["tweet"]);
- $sql = "DELETE FROM heroku_a7bcbc3dd84756e.single_stock_selected";
- mysqli_query($connect, $sql);
+ //$sql = "DELETE FROM heroku_a7bcbc3dd84756e.single_stock_selected";
+ //mysqli_query($connect, $sql);
  $sql = "INSERT INTO heroku_a7bcbc3dd84756e.single_stock_selected (Ticker) VALUES ('".$tweet."')";
  mysqli_query($connect, $sql);
 }
