f7cc10569f08d00237663b201db244a8d70f6c82
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 00:32:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Trading Results: 1</a>
+			<a href="#" class="brand-logo center">Trading Results: </a>
 			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
 		</div>
 	</nav>
@@ -139,7 +139,7 @@
 							$(document).ready(function(){
 								 //$('#tweet_button').click(function(){
 								  //var tweet_txt = $('#tweet').val();
-								  var tweet_txt = "IVW";
+								  var tweet_txt = txt;
 								  //trim() is used to remover spaces
 								  if($.trim(tweet_txt) != '')
 								  {
@@ -150,7 +150,7 @@
 								    dataType:"text",
 								    success:function(data)
 								    {
-								     //$('#tweet').val("");
+								     	txt = "";
 								    }
 								   });
 								  }
