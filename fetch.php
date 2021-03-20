c86e3b299f79daf9d29134c0cfa7e9343a2d557b
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 23:15:45 +0800

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,6 @@
 <?php
 //fetch.php
-$connect = mysqli_connect("heroku_a7bcbc3dd84756e", "b11d6e54534643", "318fd8ce", "us-cdbr-east-03.cleardb.com");
+$connect = mysqli_connect("us-cdbr-east-03.cleardb.com", "b11d6e54534643", "318fd8ce", "heroku_a7bcbc3dd84756e");
 $query = "SELECT * FROM heroku_a7bcbc3dd84756e.tbl_tweet ORDER BY tweet_id DESC";
 $result = mysqli_query($connect, $query);
 if(mysqli_num_rows($result) > 0)
