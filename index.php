f03cbb082ce9478c22012d91b0ce0a6992d16f46
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 01:46:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -235,7 +235,7 @@
 	$connect = mysqli_connect("us-cdbr-east-03.cleardb.com","b11d6e54534643","318fd8ce","heroku_a7bcbc3dd84756e");
 	$query = "SELECT * FROM heroku_a7bcbc3dd84756e.single_stock_selected";
 	$result0 = mysqli_query($connect, $query);
-	$row = mysqli_fetch_array($result0)
+	$row0 = mysqli_fetch_array($result0)
 	/*$ticker = $row["Ticker"];*/
 	
 	/* Database connection settings */
