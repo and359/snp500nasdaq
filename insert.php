1da258e581464a3ad659f50beeae6f1290f63406
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 23:15:04 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 //insert.php
 if(isset($_POST["tweet"]))
 {
- $connect = mysqli_connect("heroku_a7bcbc3dd84756e", "b11d6e54534643", "318fd8ce", "us-cdbr-east-03.cleardb.com");
+ $connect = mysqli_connect("us-cdbr-east-03.cleardb.com", "b11d6e54534643", "318fd8ce", "heroku_a7bcbc3dd84756e");
  $tweet = mysqli_real_escape_string($connect, $_POST["tweet"]);
  $sql = "INSERT INTO heroku_a7bcbc3dd84756e.tbl_tweet (tweet) VALUES ('".$tweet."')";
  mysqli_query($connect, $sql);
