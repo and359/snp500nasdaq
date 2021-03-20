1a467d5c571931f91143588bb3adebf104b2cceb
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 00:38:06 +0800

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,13 +1,13 @@
 <?php
 //fetch.php
 $connect = mysqli_connect("us-cdbr-east-03.cleardb.com", "b11d6e54534643", "318fd8ce", "heroku_a7bcbc3dd84756e");
-$query = "SELECT * FROM heroku_a7bcbc3dd84756e.tbl_tweet ORDER BY tweet_id DESC";
+$query = "SELECT * FROM heroku_a7bcbc3dd84756e.single_stock_selected";
 $result = mysqli_query($connect, $query);
 if(mysqli_num_rows($result) > 0)
 {
  while($row = mysqli_fetch_array($result))
  {
-  echo '<p>'.$row["tweet"].'</p>';
+  echo '<p>'.$row["Ticker"].'</p>';
  }
 }
 ?>
