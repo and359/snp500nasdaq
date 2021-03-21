4933edbb7ea7534d1f81b04ce538c8d1871231b4
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 19:24:20 +0800

    Update fetch_date.php

--- fetch_date.php
+++ fetch_date.php
@@ -1,18 +1,18 @@
 <?php								
-								if(isset($_POST["tweet2"])){
+								if(isset($_POST["tweet3"])){
 								
 								$host = 'us-cdbr-east-03.cleardb.com';
 								$user = 'b8a00bf633cf68';
 								$pass = '1a8113a0';
 								$db = 'heroku_69459908ed082cc';
 								$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-								$tweet1 = mysqli_real_escape_string($mysqli, $_POST["tweet2"]);
+								$tweet1 = mysqli_real_escape_string($mysqli, $_POST["tweet3"]);
 									
 								
 								$date2 = '';
 
 								//query to get data from the table
-								$sql = "SELECT * FROM `backtest` WHERE Ticker = '".$tweet2."';";
+								$sql = "SELECT * FROM `backtest` WHERE Ticker = '".$tweet3."';";
 								$result = mysqli_query($mysqli, $sql);
 
 								//loop through the returned data
