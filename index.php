6af1803d241ec23c3819f7757f9bc033409e0b3d
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 11:04:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Trading Results: 1</a>
+			<a href="#" class="brand-logo center">Trading Results: </a>
 			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
 		</div>
 	</nav>
@@ -168,11 +168,12 @@
 								//var chart = new Chart(ctx, {
 								   type: 'line',
 								   data: {
-									//labels: [<?php echo $date; ?>],
-									   labels: ["17/03/2020","13/04/2020","01/05/2020"],
+									labels: [<?php echo $date2; ?>],
+									   //labels: ["17/03/2020","13/04/2020","01/05/2020"],
 								      datasets: [{
 									 label: 'Close Price',
-									 data: [100,200,300],
+									 //data: [100,200,300],
+									 data: [<?php echo $data9; ?>],
 									 backgroundColor: 'rgba(0, 119, 290, 0.2)',
 									 borderColor: 'rgba(0, 119, 290, 0.6)'
 								      }]
