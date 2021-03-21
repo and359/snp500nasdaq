ab8a1bc53ac8af8e4a7a6ca11f674dfd9b0a0049
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 12:25:42 +0800

    Update fetch_ticker.php

--- fetch_ticker.php
+++ fetch_ticker.php
@@ -1,15 +1,18 @@
 <?php								
+								if(isset($_POST["tweet1"])){
+								
 								$host = 'us-cdbr-east-03.cleardb.com';
 								$user = 'b8a00bf633cf68';
 								$pass = '1a8113a0';
 								$db = 'heroku_69459908ed082cc';
 								$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-								
+								$tweet = mysqli_real_escape_string($mysqli, $_POST["tweet1"]);
+									
 								$date2 = '';
 								$data9 = '';
 
 								//query to get data from the table
-								$sql = "SELECT * FROM `backtest` WHERE Ticker = '".tweet_txt."';";								
+								$sql = "SELECT * FROM `backtest` WHERE Ticker = '".$tweet1."';";
 								$result = mysqli_query($mysqli, $sql);
 
 								//loop through the returned data
@@ -22,5 +25,6 @@
 								$date2 = trim($date2,",");
 								$data9 = trim($data9,",");
                 
-                echo $data9;
+                						echo $data9;
+								}
 								?>
