b61f093ab307cb1da18f24d90a88f41bebec9cca
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 14:48:29 +0800

    Create fetch_date.php

---
+++ fetch_date.php
@@ -0,0 +1,28 @@
+<?php								
+								if(isset($_POST["tweet2"])){
+								
+								$host = 'us-cdbr-east-03.cleardb.com';
+								$user = 'b8a00bf633cf68';
+								$pass = '1a8113a0';
+								$db = 'heroku_69459908ed082cc';
+								$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+								$tweet1 = mysqli_real_escape_string($mysqli, $_POST["tweet2"]);
+									
+								$date2 = '';
+
+								//query to get data from the table
+								$sql = "SELECT * FROM `backtest` WHERE Ticker = '".$tweet2."';";
+								$result = mysqli_query($mysqli, $sql);
+
+								//loop through the returned data
+								while ($row = mysqli_fetch_array($result)) {
+
+									$date2 = $date2 . '"'. $row['PriceDate'] .'",';		
+								}
+
+								$date2 = trim($date2,",");
+								
+                
+                						echo $date2;
+								}
+								?>
