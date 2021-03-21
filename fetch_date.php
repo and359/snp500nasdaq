d12c84d9ba279fb7f6414e6827292366310a123c
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 18:26:35 +0800

    Update fetch_date.php

--- fetch_date.php
+++ fetch_date.php
@@ -22,12 +22,6 @@
 
 								$date2 = trim($date2,",");
 								
-                						echo $date2;
-								//file_put_contents('mydate.txt', $date2, FILE_APPEND | LOCK_EX);
-									
-								/*$myfile = fopen("mydate.txt", "w") or die("Unable to open file!");								
-								//fwrite($myfile, $date2);
-								file_put_contents($myfile, $date2);
-								fclose($myfile);*/
+                						echo $date2;								
 								}
 								?>
