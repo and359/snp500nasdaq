4c796196169508f38114ea0f9a07739eda55080a
and359 <79313508+and359@users.noreply.github.com>
2021-03-21 23:13:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -239,7 +239,7 @@
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Trading Results: 1</a>
+			<a href="#" class="brand-logo center">Trading Results: </a>
 			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
 		</div>
 	</nav>
@@ -442,8 +442,8 @@
 								      },
 								      annotation: {
 									 //drawTime: 'afterDatasetsDraw',
-									 drawTime: 'afterDraw'//,
-									 //annotations: testETSY
+									 drawTime: 'afterDraw',
+									 annotations: testETSY
 								      }
 								   }
 						});} else if (txt=='TSLA '){
@@ -476,8 +476,8 @@
 								      },
 								      annotation: {
 									 //drawTime: 'afterDatasetsDraw',
-									 drawTime: 'afterDraw'//,
-									 //annotations: testTSLA
+									 drawTime: 'afterDraw',
+									 annotations: testTSLA
 								      }
 								   }
 						});}	else {																
@@ -509,8 +509,8 @@
 								      },
 								      annotation: {
 									 //drawTime: 'afterDatasetsDraw',
-									 drawTime: 'afterDraw'//,
-									 //annotations: testIVW
+									 drawTime: 'afterDraw',
+									 annotations: testIVW
 								      }
 								   }
 						});}	
